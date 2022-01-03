e9e4da3ae9b26ca92b5f02a86eb9d3dfac2ed143
Mel Grosser <m.grosser@curasoft.de>
2022-01-03 15:58:18 +0100

    initial commit

---
+++ src/components/LandingHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <nav
+      class="w-full bg-white text-gray-800 dark:bg-gray-800 dark:text-white py-8 px-8 shadow-md dark:shadow-md flex items-center"
+    >
+      <icon-ph:chat-teardrop-text class="text-5xl" />
+      <router-link :to="{ name: 'home' }" class="ml-6">
+        <div
+          class="font-bold font-['Roboto'] lg:text-3xl md:text-lg text-md tracking-wide italic"
+        >triller</div>
+      </router-link>
+      <div class="ml-auto flex items-center h-full">
+        <button class="btn glass mr-6">Sign up / Sign In</button>
+        <a
+          target="_blank"
+          referrerpolicy="no-referrer"
+          href="https://github.com/zynth17/vitailse"
+        >
+          <icon-akar-icons:github-fill />
+        </a>
+      </div>
+    </nav>
+  </header>
+</template>
---
+++ src/layouts/landingLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <header>
+    <LandingHeader />
+  </header>
+
+  <main
+    class="py-8 xl:px-40 lg:px-20 dark:bg-gray-800 dark:text-white bg-white text-gray-800 min-h-screen"
+  >
+    <router-view />
+  </main>
+</template>
